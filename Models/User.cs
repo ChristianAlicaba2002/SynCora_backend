23247af74376658e32e0341019e6fcc9b832dcbe
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-26 11:56:13 +0800

    feat: Add User model with properties for user management in the application

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace syncora_server.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string MiddleName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdateddAt { get; set; }
+}
