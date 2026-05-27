575319ad7630f9762b82d74a037b5d6168e50262
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-27 12:25:32 +0800

    feat: Add UserDTOs for user registration and login data transfer objects

---
+++ DTOs/UserDTOs.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace syncora_server.DTOs;
+
+public class UserDTOs
+{
+    public class RegisterUserDTOs
+    {
+        public required string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public required string LastName { get; set; }
+        public required string Gender { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+
+    public class LoginUserDTOs
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+
+}
