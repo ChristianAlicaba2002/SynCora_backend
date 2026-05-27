ea0d4ad44d74515ca648eaeaecdb6c2a53aa79e2
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-27 12:26:44 +0800

    feat: Update User model to include optional MiddleName, Gender, Bio, and ImageUrl properties

--- Models/User.cs
+++ Models/User.cs
@@ -6,10 +6,14 @@ public class User
 {
     public Guid Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
-    public string MiddleName { get; set; } = string.Empty;
+    public string? MiddleName { get; set; }
     public string LastName { get; set; } = string.Empty;
+    public string Gender { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
+    // public List<Guid> Friends { get; set; } = [];
+    public string ImageUrl {get;set;} = string.Empty;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdateddAt { get; set; }
 }
