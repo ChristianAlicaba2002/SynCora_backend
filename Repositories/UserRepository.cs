5872cd46d55932fcb5999f1288f26e0b6af449e4
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-27 12:27:15 +0800

    feat: Implement UserRepository for user management, including user registration and retrieval by email

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using syncora_server.Data;
+using syncora_server.DTOs;
+using syncora_server.Interface.IUser;
+using syncora_server.Models;
+
+namespace syncora_server.Repositories;
+
+public class UserRepository(AppDbContext appDb) : IUserRepository
+{
+    private readonly AppDbContext _db = appDb;
+
+    private static string Capitalize(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value)) return string.Empty;
+        value = value.Trim();
+        return char.ToUpper(value[0]) + value.Substring(1).ToLower();
+    }
+
+    public async Task<User?> GetUserEmail(string email)
+    {
+        var userEmail = await _db.Users.FirstOrDefaultAsync(user => user.Email == email);
+
+        return userEmail;
+    }
+
+    public async Task RegisterUser(UserDTOs.RegisterUserDTOs _registerUserDTOs)
+    {
+        var saveUser = new User
+        {
+            Id = Guid.NewGuid(),
+            FirstName = Capitalize(_registerUserDTOs.FirstName),
+            MiddleName = Capitalize(_registerUserDTOs.MiddleName),
+            LastName = Capitalize(_registerUserDTOs.LastName),
+            Gender = Capitalize(_registerUserDTOs.Gender),
+            Email = _registerUserDTOs.Email.Trim(),
+            Password = BCrypt.Net.BCrypt.HashPassword(_registerUserDTOs.Password.Trim()),
+            CreatedAt = DateTime.Now,
+            UpdateddAt = DateTime.Now,
+        };
+
+        _db.Users.Add(saveUser);
+        await _db.SaveChangesAsync();
+    }
+}
