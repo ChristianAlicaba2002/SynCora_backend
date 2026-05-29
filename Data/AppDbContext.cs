0d0988754b7f3290c5bc4073b0c02196e53fa752
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-29 14:36:36 +0800

    feat: Add Tasks and Activities tables to AppDbContext with corresponding migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,5 +9,7 @@ public class AppDbContext: DbContext
 {
    public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
 
-   public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Tasks> Tasks { get; set; }
+    public DbSet<Activity> Activities { get; set; }
 }
--- Migrations/20260529063015_AddedNewTwoTables.Designer.cs
+++ Migrations/20260529063015_AddedNewTwoTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using syncora_server.Data;
 
@@ -11,9 +12,11 @@ using syncora_server.Data;
 namespace syncora_server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260529063015_AddedNewTwoTables")]
+    partial class AddedNewTwoTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
