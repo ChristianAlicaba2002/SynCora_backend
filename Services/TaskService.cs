69681b6eec93fc78f339ff39efa7e9896d414646
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-30 13:39:32 +0800

    feat: Implement TaskService class for task creation functionality

---
+++ Services/TaskService.cs
@@ -0,0 +1,12 @@
+using syncora_server.Interface.ITask;
+using syncora_server.Models;
+
+namespace syncora_server.Services;
+
+public class TaskService : ITaskService
+{
+    public Task<Tasks> CreateTask()
+    {
+        throw new NotImplementedException();
+    }
+}
