67d7ec2a0a268a6f0bea9bee2e7bd86447f7da4e
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-08 17:59:03 +0800

    feat(UserRepository): Add user retrieval and profile update methods
    - Implement GetUserById method to fetch user details by their unique identifier.
    - Add UpdateUserProfile method to allow updates to user profiles using a DTO and user ID, ensuring partial updates are handled correctly.

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -24,6 +24,11 @@ public class UserRepository(AppDbContext context) : IUserRepository
         return userEmail;
     }
 
+    public async Task<User?> GetUserById(Guid id)
+    {
+        return await _context.Users.FirstOrDefaultAsync(user => user.Id == id);
+    }
+
     public async Task RegisterUser(UsersDTOs.RegisterUserDTOs _registerUserDTOs)
     {
         var saveUser = new User
@@ -40,6 +45,24 @@ public class UserRepository(AppDbContext context) : IUserRepository
         };
 
         _context.Users.Add(saveUser);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateUserProfile(UsersDTOs.UpdateUserDTO _updateUserDTO, Guid userId)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+        if (user is null) return;
+
+        user.FirstName = Capitalize(_updateUserDTO.FirstName) is "" ? user.FirstName : Capitalize(_updateUserDTO.FirstName);
+        user.MiddleName = _updateUserDTO.MiddleName ?? user.MiddleName;
+        user.LastName = Capitalize(_updateUserDTO.LastName) is "" ? user.LastName : Capitalize(_updateUserDTO.LastName);
+        user.Gender = Capitalize(_updateUserDTO.Gender) is "" ? user.Gender : Capitalize(_updateUserDTO.Gender);
+        user.Email = string.IsNullOrWhiteSpace(_updateUserDTO.Email) ? user.Email : _updateUserDTO.Email.Trim();
+        user.Bio = string.IsNullOrWhiteSpace(_updateUserDTO.Bio) ? user.Bio : _updateUserDTO.Bio;
+        user.ImageUrl = string.IsNullOrWhiteSpace(_updateUserDTO.ImageUrl) ? user.ImageUrl : _updateUserDTO.ImageUrl;
+        user.UpdateddAt = DateTime.Now;
+
         await _context.SaveChangesAsync();
     }
 }
