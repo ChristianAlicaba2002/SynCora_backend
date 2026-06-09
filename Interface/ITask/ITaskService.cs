83e3e20cebc589e6710a47f1f83f9574807717f4
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-09 11:03:07 +0800

    feat(ITaskService): Add GetAllTasksAsync method for asynchronous task retrieval

--- Interface/ITask/ITaskService.cs
+++ Interface/ITask/ITaskService.cs
@@ -4,6 +4,7 @@ namespace syncora_server.Interface.ITask;
 
 public interface ITaskService
 {
+    Task<List<TaskResponseDTO>> GetAllTasksAsync();
     Task<TaskResponseDTO> CreateTaskAsync(CreateTaskDTO dto);
     Task<List<TaskResponseDTO>> GetMyTasksAsync();
     Task<TaskResponseDTO?> GetTaskByIdAsync(Guid taskId);
