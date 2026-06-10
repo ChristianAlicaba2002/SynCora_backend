e2ef1d53ef4bd28a3c54e9cbf242a944f61c0ab9
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-10 11:07:43 +0800

    feat(Models): Enhance User model with follow request and relationship collections
    - Add collections for SentFollowRequests, ReceivedFollowRequests, Following, and Followers to the User model.
    - Initialize new collections to manage user follow interactions effectively.

--- Models/User.cs
+++ Models/User.cs
@@ -13,7 +13,19 @@ public class User
     public string Password { get; set; } = string.Empty;
     public string Bio { get; set; } = string.Empty;
     // public List<Guid> Friends { get; set; } = [];
-    public string ImageUrl {get;set;} = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdateddAt { get; set; }
+
+    public ICollection<FollowRequest> SentFollowRequests { get; set; }
+        = new List<FollowRequest>();
+
+    public ICollection<FollowRequest> ReceivedFollowRequests { get; set; }
+        = new List<FollowRequest>();
+
+    public ICollection<Follow> Following { get; set; }
+        = new List<Follow>();
+
+    public ICollection<Follow> Followers { get; set; }
+        = new List<Follow>();
 }
