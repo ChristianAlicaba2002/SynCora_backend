145244b8d08c35092cb95ff686f5e19882013971
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-12 11:07:31 +0800

    feat(Repositories): Add method to retrieve user follow requests
    - Introduce GetUserFollowRequestAsync method in IFollowRepository to fetch follow requests for a specific user.
    - Enhance the repository interface to support follow request retrieval, improving user interaction capabilities.

--- Interface/IFollow/IFollowRepository.cs
+++ Interface/IFollow/IFollowRepository.cs
@@ -1,7 +1,10 @@
+using syncora_server.DTOs;
+
 namespace syncora_server.Interface.IFollow;
 
 public interface IFollowRepository
 {
+    Task<List<FollowRequestResponseDto>> GetUserFollowRequestAsync(Guid userId);
     Task SendFollowRequestAsync(Guid senderId, Guid receiverId);
     Task AcceptFollowRequestAsync(Guid requestId, Guid receiverId);
 }
