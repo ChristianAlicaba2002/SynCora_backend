ce4e070f46c7f599a3e39b83e36d85f3a2923d02
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-12 11:07:37 +0800

    feat(Interface): Add GetUserFollowRequestAsync method to IFollowService
    - Introduce GetUserFollowRequestAsync method in IFollowService to retrieve follow requests for a user, enhancing the service's functionality for managing follow interactions.

--- Interface/IFollow/IFollowService.cs
+++ Interface/IFollow/IFollowService.cs
@@ -1,7 +1,10 @@
+using syncora_server.DTOs;
+
 namespace syncora_server.Interface.IFollow;
 
 public interface IFollowService
 {
+    Task<List<FollowRequestResponseDto>> GetUserFollowRequestAsync();
     Task SendFollowRequestAsync(Guid receiverId);
     Task AcceptFollowRequestAsync(Guid requestId);
 }
