d55f2b2d64396b0a1e0bb065ce19642eac5cd9cf
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-15 09:36:02 +0800

    feat: implement user authentication services and social follow repository patterns

--- Interface/IFollow/IFollowRepository.cs
+++ Interface/IFollow/IFollowRepository.cs
@@ -7,4 +7,6 @@ public interface IFollowRepository
     Task<List<FollowRequestResponseDto>> GetUserFollowRequestAsync(Guid userId);
     Task SendFollowRequestAsync(Guid senderId, Guid receiverId);
     Task AcceptFollowRequestAsync(Guid requestId, Guid receiverId);
+    Task UnfollowAsync(Guid followerId, Guid followingId);
+    Task CancelFollowRequestAsync(Guid senderId, Guid receiverId);
 }
