fa49decc180439b7201256841ef6ba54ea1d3c51
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-15 09:36:09 +0800

    feat: implement user and follow service/repository layers with dependency injection

--- Interface/IFollow/IFollowService.cs
+++ Interface/IFollow/IFollowService.cs
@@ -7,4 +7,6 @@ public interface IFollowService
     Task<List<FollowRequestResponseDto>> GetUserFollowRequestAsync();
     Task SendFollowRequestAsync(Guid receiverId);
     Task AcceptFollowRequestAsync(Guid requestId);
+    Task UnfollowAsync(Guid followingId);
+    Task CancelFollowRequestAsync(Guid receiverId);
 }
