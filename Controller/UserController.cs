239c4337a09b2953bf630a478f8db0a43ef578c7
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-16 11:35:45 +0800

    feat: add endpoints to retrieve user followers and following counts

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -102,4 +102,20 @@ public class UserControllerController(IUserService iUserService, ILogger<UserCon
         var users = await _iUserService.SearchUser(searchQuery);
         return Ok(new { message = "Users searched successfully.", status = StatusCodes.Status200OK, data = users });
     }
+
+    [Authorize(Policy = "UserOnly")]
+    [HttpGet("followers-count")]
+    public async Task<IActionResult> GetUserFollowersCount()
+    {
+        var followersCount = await _iUserService.GetUserFollowersCount();
+        return Ok(new { message = "User followers count retrieved successfully.", status = StatusCodes.Status200OK, data = followersCount });
+    }
+
+    [Authorize(Policy = "UserOnly")]
+    [HttpGet("following-count")]
+    public async Task<IActionResult> GetUserFollowingCount()
+    {
+        var followingCount = await _iUserService.GetUserFollowingCount();
+        return Ok(new { message = "User following count retrieved successfully.", status = StatusCodes.Status200OK, data = followingCount });
+    }
 }
